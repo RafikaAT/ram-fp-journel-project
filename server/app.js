01308a1f4649af1b7f95633f0cd7c0950d4eed9c
Velo <hartley.max@outlook.com>
2021-07-05 10:57:22 +0100

    create basic server

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+// create server
+const app = express();
+
+// add middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.status(200).send({ message: 'Hello World!' });
+});
+
+module.exports = app;
