aa4c403b3487ae5b3e561e85b92d459fd25061ef
Rafika <RTuremis@hotmail.co.uk>
2021-07-05 18:04:52 +0100

    Create navbar and button to submission page on homepage via HTML and CSS

---
+++ client/assets/js/index.js
@@ -0,0 +1,1 @@
+console.error("Error: Can't walk dependency graph: Cannot find module '/Users/rafikaturemis/Documents/journal_project/ram-fp-journel-project/client/assets/bundle.js' from '/Users/rafikaturemis/Documents/journal_project/ram-fp-journel-project/client/assets/_fake.js'\n    required by /Users/rafikaturemis/Documents/journal_project/ram-fp-journel-project/client/assets/_fake.js");
