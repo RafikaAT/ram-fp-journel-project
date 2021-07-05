d99a3c1d2bc5f46aa482da0b761b2286af83eb10
Velo <hartley.max@outlook.com>
2021-07-05 22:42:32 +0100

    abstract file writing to data.js file and refactor comment model

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,6 +1,4 @@
-const fs = require('fs');
-const jsonData = fs.readFileSync('../data.json');
-const data = JSON.parse(jsonData);
+const { readDataFromFile, writeDataToFile } = require('./data');
 
 class Comment {
 	constructor(comment) {
@@ -10,45 +8,79 @@ class Comment {
 		this.journalId = comment.journalId;
 	}
 
+	static getAllData() {
+		const allData = readDataFromFile();
+		return allData;
+	}
+
 	static createNewId() {
+		const data = this.getAllData();
 		const allIds = data.comments.map((comment) => comment.id);
 		const newId = Math.max(Math.max(...allIds) + 1, 0);
 		return newId;
 	}
 
 	static all() {
+		const data = this.getAllData();
 		const allComments = data.comments.map((comment) => new Comment(comment));
 		return allComments;
 	}
 
 	static createNewComment(comment) {
+		const data = this.getAllData();
 		const newCommentId = this.createNewId();
-		const newComment = new Comment({ id: newCommentId, ...comment });
-		data.comments.push(newComment);
-		return newComment;
+		const newCommentData = { id: newCommentId, ...comment };
+		const newComment = new Comment(newCommentData);
+		data.comments.push(newCommentData);
+		const hasFileBeenUpdated = writeDataToFile(data);
+		if (hasFileBeenUpdated) {
+			return newComment;
+		} else {
+			throw new Error('Data not written to file.');
+		}
 	}
 
 	static deleteCommentById(commentIdToDelete) {
+		const data = this.getAllData();
 		const comment = this.findCommentById(commentIdToDelete);
 		if (!comment) return null;
 		data.comments = data.comments.filter((comment) => comment.id !== commentIdToDelete);
-		return true;
+		const hasFileBeenUpdated = writeDataToFile(data);
+		if (hasFileBeenUpdated) {
+			return true;
+		} else {
+			throw new Error('Data not written to file.');
+		}
 	}
 
-	static updateComment(newComment) {
-		const commentIndex = data.comments.findIndex((comment) => comment.id === newComment.id);
-		data.comments[commentIndex] = newComment;
-		return new Comment(newComment);
+	static updateComment(newCommentData) {
+		const data = this.getAllData();
+		const commentIndex = data.comments.findIndex((comment) => comment.id === newCommentData.id);
+		data.comments[commentIndex] = newCommentData;
+		const newComment = new Comment(newCommentData);
+		const hasFileBeenUpdated = writeDataToFile(data);
+		if (hasFileBeenUpdated) {
+			return newComment;
+		} else {
+			throw new Error('Data not written to file.');
+		}
 	}
 
 	static findCommentById(idToFind) {
+		const data = this.getAllData();
 		const comment = data.comments.filter((comment) => idToFind === comment.id)[0];
 		return new Comment(comment) || null;
 	}
 
 	static deleteAllCommentsInJournal(journalId) {
+		const data = this.getAllData();
 		data.comments = data.comments.filter((comment) => comment.journalId !== journalId);
-		return true;
+		const hasFileBeenUpdated = writeDataToFile(data);
+		if (hasFileBeenUpdated) {
+			return true;
+		} else {
+			throw new Error('Data not written to file.');
+		}
 	}
 }
 
