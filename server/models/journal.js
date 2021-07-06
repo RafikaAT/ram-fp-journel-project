332faff74d020e0bd12f095515420898c53c77a8
akashkhambay <akashkhambay@hotmail.co.uk>
2021-07-06 10:34:29 +0100

    Add write and read data functions to journal.js

--- server/models/journal.js
+++ server/models/journal.js
@@ -1,6 +1,9 @@
-const fs = require('fs');
-const jsonData = fs.readFileSync('../data.json');
-const data = JSON.parse(jsonData);
+// const fs = require('fs');
+// const jsonData = fs.readFileSync('../data.json');
+// const data = JSON.parse(jsonData);
+
+const { readDataFromFile, writeDataToFile } = require('./data'); 
+
 
 class Journal {
 	constructor(journal) {
@@ -9,6 +12,14 @@ class Journal {
 		this.id = journal.id;
 		this.comments = journal.comments || [];
 	}
+	writeNewJournalDataToFile(data) {
+		const hasFileBeenUpdated = writeDataToFile(data);
+		if (!hasFileBeenUpdated) throw new Error('Data not written to file.');
+	}
+
+	
+
+
 // Akash is now going to try and attempt to make a function that will add journal entries the journal object.
 	static createNewJournalEntry(journal) {
 		const newJournalId = createNewId();
