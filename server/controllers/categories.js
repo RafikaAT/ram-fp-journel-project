020939e53da59dcef3412acf9018d37353aa5297
Rafika <RTuremis@hotmail.co.uk>
2021-07-07 16:50:45 +0100

    Make route for journals by category, implement API key, improve data for Postman route testing

--- server/controllers/categories.js
+++ server/controllers/categories.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const categoriesRouter = express.Router();
+const Journal = require('../models/journal');
 
 categoriesRouter.get('/', (req, res) => {
-	// TODO get all categories and return them
-	res.status(200).send({ categories: 'All categories' });
+  // TODO get all categories and return them
+  res.status(200).send({ categories: 'All categories' });
 });
 
 categoriesRouter.get('/:category', (req, res) => {
-	const { category } = req.params;
-	// TODO get all journal entries in category and return them
-	res.status(200).send({ journalEntries: `All journal entries in ${category}` });
+  try {
+    const category = req.params.category;
+    const journalsInCategory = Journal.getJournalsByCategory(category);
+    res.status(200).send({ journalEntries: journalsInCategory });
+  } catch (err) {
+    res.status(500).send;
+  }
 });
 
 module.exports = categoriesRouter;
