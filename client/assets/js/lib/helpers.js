7b2653e734231ec3d229288c55749bd6729c1a54
Velo <hartley.max@outlook.com>
2021-07-08 16:07:32 +0100

    add emoji reaction handling js

--- client/assets/js/lib/helpers.js
+++ client/assets/js/lib/helpers.js
@@ -30,7 +30,7 @@ async function createJournalHTML({ id, title, content, giphyData, emojis, commen
 	journalContent.textContent = content;
 	journalArticle.append(journalContent);
 
-	const emojisList = createEmojisHTML(emojis);
+	const emojisList = createEmojisHTML(emojis, true, id);
 	journalArticle.append(emojisList);
 
 	if (giphyData) {
@@ -69,7 +69,7 @@ function createCommentHtml({ id, comment, giphyData, emojis }) {
 	commentBody.textContent = comment;
 	commentDiv.append(commentBody);
 
-	const emojisList = createEmojisHTML(emojis);
+	const emojisList = createEmojisHTML(emojis, false, id);
 	commentDiv.append(emojisList);
 
 	if (giphyData) {
@@ -83,15 +83,34 @@ function createCommentHtml({ id, comment, giphyData, emojis }) {
 }
 
 // TODO: add click event listeners to emojis to update data
-function createEmojisHTML({ likes, loves, dislikes }) {
+function createEmojisHTML({ likes, loves, dislikes }, isJournal, id) {
+	addEmojiToLocalStorage(id);
 	const emojisList = document.createElement('ul');
+	emojisList.className = `${id} ${isJournal}`;
 
 	const likesLi = document.createElement('li');
 	likesLi.textContent = `👍: ${likes}`;
+	likesLi.classList.add('likes');
+	if (getEmojiState('likes', id)) {
+		likesLi.classList.add('emoji-checked');
+	}
+	likesLi.addEventListener('click', handleEmojiClick);
+
 	const lovesLi = document.createElement('li');
 	lovesLi.textContent = `❤: ${loves}`;
+	lovesLi.classList.add('loves');
+	if (getEmojiState('loves', id)) {
+		lovesLi.classList.add('emoji-checked');
+	}
+	lovesLi.addEventListener('click', handleEmojiClick);
+
 	const dislikesLi = document.createElement('li');
 	dislikesLi.textContent = `👎: ${dislikes}`;
+	dislikesLi.classList.add('dislikes');
+	if (getEmojiState('dislikes', id)) {
+		dislikesLi.classList.add('emoji-checked');
+	}
+	dislikesLi.addEventListener('click', handleEmojiClick);
 
 	emojisList.append(likesLi);
 	emojisList.append(lovesLi);
@@ -100,3 +119,91 @@ function createEmojisHTML({ likes, loves, dislikes }) {
 	return emojisList;
 }
 module.exports = { createAllJournals };
+
+function updateEmojiLocalStorage(emojiToUpdate, id) {
+	const { localStorage } = window;
+	let emojis = JSON.parse(localStorage.getItem('emojis'));
+	// find index of emoji to update in stored array
+	emojiIndex = emojis.emojis.findIndex((item) => item.id === id);
+	const emojiToStore = {
+		likes: false,
+		loves: false,
+		dislikes: false,
+		id: id,
+	};
+	if (emojiIndex >= 0) {
+		emojis.emojis[emojiIndex][emojiToUpdate] = !emojis.emojis[emojiIndex][emojiToUpdate];
+	} else {
+		emojis.emojis.push(emoji);
+	}
+
+	localStorage.setItem('emojis', JSON.stringify(emojis));
+	return emojis.emojis[emojiIndex][emojiToUpdate];
+}
+
+function addEmojiToLocalStorage(id) {
+	const { localStorage } = window;
+	let emojis = JSON.parse(localStorage.getItem('emojis'));
+	// if emojis array doesn't exist in local storage create it
+	if (!emojis) {
+		localStorage.setItem('emojis', JSON.stringify({ emojis: [] }));
+		emojis = localStorage.getItem('emojis');
+	}
+
+	// if emoji is not in local storage add it.
+	emojiIndex = emojis.emojis.findIndex((item) => item.id === id);
+	if (emojiIndex === -1) {
+		const emojiToStore = {
+			likes: false,
+			loves: false,
+			dislikes: false,
+			id: id,
+		};
+		emojis.emojis.push(emojiToStore);
+		localStorage.setItem('emojis', JSON.stringify(emojis));
+	}
+}
+
+function getEmojiState(emoji, id) {
+	const { localStorage } = window;
+	let emojis = JSON.parse(localStorage.getItem('emojis'));
+	emojiIndex = emojis.emojis.findIndex((item) => item.id === id);
+	return emojis.emojis[emojiIndex][emoji];
+}
+
+async function handleEmojiClick(e) {
+	e.target.textContent.split(':');
+	const emojiClickedInfo = e.target.textContent.split(':');
+	const emojiClicked =
+		emojiClickedInfo[0] === '👍' ? 'likes' : emojiClickedInfo[0] === '❤' ? 'loves' : 'dislikes';
+
+	const [parentId, isParentJournal] = e.target.parentNode.classList;
+	let journalId =
+		isParentJournal === 'true' ? parentId : e.target.parentNode.parentNode.parentNode.parentNode.id;
+
+	const url = `${urlInfo.backEnd}journals/${journalId}/${
+		isParentJournal === 'true' ? emojiClicked : `comments/${parentId}/${emojiClicked}`
+	}`;
+
+	const isEmojiChecked = getEmojiState(emojiClicked, parentId);
+	const requestBody = {
+		isEmojiChecked,
+	};
+
+	const updatedEntry = await putDataToApi(url, requestBody);
+	const emoji = document.querySelector(`#${parentId} .${e.target.classList[0]}`);
+	const newEmojiCount =
+		isParentJournal === 'true'
+			? updatedEntry.journal.emojis[emojiClicked]
+			: updatedEntry.comment.emojis[emojiClicked];
+	emojiClickedInfo[1] = newEmojiCount;
+	emoji.textContent = emojiClickedInfo.join(':');
+
+	const newState = updateEmojiLocalStorage(emojiClicked, parentId);
+
+	if (newState) {
+		emoji.classList.add('emoji-checked');
+	} else {
+		emoji.classList.remove('emoji-checked');
+	}
+}
