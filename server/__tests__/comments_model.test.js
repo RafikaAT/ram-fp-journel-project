98b329838dea3ee39b77257a01c59d5fc6af730a
akashkhambay <akashkhambay@hotmail.co.uk>
2021-07-09 11:22:29 +0100

    Add tests to comments model, change script from jest to coverage

--- server/__tests__/comments_model.test.js
+++ server/__tests__/comments_model.test.js
@@ -31,4 +31,33 @@ describe("comments model", () => {
     const allData = Comment.getAllCommentsInJournal();
     expect(testData.comments.length).toBe(0);
   });
+
+  test("Method all should get all comments", () => {
+    const allData = Comment.all();
+    expect(allData.length).toBe(testData.comments.length);
+  });
+
+  test("findCommentById should return an object which has matching id key value pair, if exists", () => {
+    let testDataId = "test-comment-id";
+    const foundComment = Comment.findCommentById(testDataId);
+    expect(foundComment).toHaveProperty("id", testDataId);
+  });
+
+  test("deleteCommentById should return the comments object with one less item", () => {
+    Comment.deleteCommentById("test-comment-id1");
+    expect(Comment.findCommentById("test-comment-id1")).toBeFalsy();
+  });
+
+  test("Update emojis should give different emoji counts", () => {
+    const originalSpecificCommentData =
+      Comment.findCommentById("test-comment-id2");
+    const originalEmojiCount = originalSpecificCommentData.emojis.likes;
+    const updatedSpecificCommentData = Comment.updateEmoji(
+      "likes",
+      true,
+      "test-comment-id2"
+    );
+    const specificEmojiCount = updatedSpecificCommentData.emojis.likes;
+    expect(originalEmojiCount).toEqual(specificEmojiCount - 1);
+  });
 });
